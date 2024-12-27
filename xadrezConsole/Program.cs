01deb1f7a33dc530c86839c2b33b9d6a0e689979
Mateus Tomaz <mstomaz11@gmail.com>
2024-12-27 11:21:42 -0300

    Criaçao da classe Posicao

--- xadrezConsole/Program.cs
+++ xadrezConsole/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace xadrezConsole
+﻿namespace xadrezConsole;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
